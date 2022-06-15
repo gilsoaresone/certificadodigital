78e24685afc64f76eb6e132f35324efdd4e90012
gilsoaresone <61061428+gilsoaresone@users.noreply.github.com>
2022-06-15 17:32:24 -0300

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,30 @@ const userSchema = require("../models/user");
 const { body, validationResult } = require("express-validator")
 const router = express.Router();
 
+
+
+
+
+
+
+const cors = require('cors');
+router.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    app.use(cors());
+    next();
+});
+
+
+
+
+
+
+
+
+
 // create user
 router.post("/agendamento", (req, res) => {
     const user = userSchema(req.body);
