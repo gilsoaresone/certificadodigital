62e62344e43e6829375e631d811660ede238d83b
gilsoaresone <61061428+gilsoaresone@users.noreply.github.com>
2022-06-21 23:50:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,14 +9,7 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 // middlewares
-app.use((req, res, next) => {
-    res.header('Acess-Control-Allow-Origin', "*");
-     res.header('Acess-Control-Allow-Origin', "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-   if(req.method ==="OPTIONS"){
-   res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE")
-       retun res.status(200).json({})
-   }
-});
+
 app.use(express.json());
 app.use("/api", userRoute);
 
