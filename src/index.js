8f319bc3e249f591430b28594fccedbe43df38c1
gilsoaresone <61061428+gilsoaresone@users.noreply.github.com>
2022-06-22 11:26:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,13 +26,18 @@ const port = process.env.PORT || 9000;
 
 app.use(express.json());
 app.use("/api", userRoute);
+          app.use(function(req, res, next) {
+          res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          next();
+          });
 
 
 
 
 
 // routes
-app.get("/", (req, res, next) => {
+app.get("/", function (req, res, next) => {
           
      
      res.send("Bem vindo a minha api");
