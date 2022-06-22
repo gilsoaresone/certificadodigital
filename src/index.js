21b83bb0352bdcbc8a171a0bb21efb1bca755105
gilsoaresone <61061428+gilsoaresone@users.noreply.github.com>
2022-06-22 11:00:47 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use("/api", userRoute);
 
 
 // routes
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "X-Requested-With");
         next();
